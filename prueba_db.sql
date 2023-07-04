4c10c652a14f87da29265e89e27e7f24214a9ae9
Brennsel <brennsell@gmail.com>
2023-07-04 18:45:55 -0300

    Se agrego ejercicio de practica

---
+++ prueba_db.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS prueba_db; /* crear solo si no existe */
+
+USE prueba_db;
+
+CREATE TABLE Casa (
+	id_casa int,
+    calle varchar(50),
+    numeracion int,
+    es_departamento boolean,
+    CONSTRAINT PK_Casa primary key (id_casa)
+);
+
+create table Usuario (
+	id_usuario int,
+    nombre varchar(10),
+    apellido varchar(10),
+    id_casa2 int,
+    constraint PK_Usuario primary key (id_usuario),
+    constraint FK_CasaUsuario foreign key (id_casa2) references Casa (id_casa)
+);
+
+insert into Casa (id_casa, calle, numeracion, es_departamento) values (163748, 'Av Libertador', 1465, 1); /* tupla */
+insert into Casa (id_casa, calle, numeracion, es_departamento) values (345678, 'Carcaraña', 3488, 0);
+
+insert into Usuario (id_usuario, nombre, apellido, id_casa2) values (854267, 'Noelia', 'Devito', 163748);
+
+SELECT *
+FROM Casa
+WHERE calle = 'Carcaraña';  /*filtrar */
+
+SELECT *
+FROM Usuario;
